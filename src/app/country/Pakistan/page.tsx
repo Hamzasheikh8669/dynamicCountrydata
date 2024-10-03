7b04159c3728e8675ba56ec3bb30efddcd38c0c2
Hey hamza sheikh here <164283922+Hamzasheikh8669@users.noreply.github.com>
2024-10-03 14:01:07 -0700

    Update page.tsx

--- src/app/country/Pakistan/page.tsx
+++ src/app/country/Pakistan/page.tsx
@@ -14,7 +14,7 @@ export default function Pakistan(){
         <Link href="/">Dashboard</Link>
       </div>
    
-      <h1 className="text-4xl mt-5 ml-10 underline bg-green-600 flex justify-center items-center italic" >Country Pakistan:</h1>
+      <h1 className="text-4xl mt-5 ml-10 underline font-bold">Country: Pakistan</h1>
       <p className="mt-3 text-2xl ml-10 text-green-600" >Name: Pakistan</p>
       <p className="mt-1 text-2xl ml-10 text-green-600" >population: 24000000</p>
       <p className="mt-1 text-2xl ml-10 text-green-600" >capital: Islamabad</p>
